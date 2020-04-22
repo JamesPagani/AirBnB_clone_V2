5083906215ef8599e175ee6204e017983e8b3f1d
JamesPagani <jaimeagalvezv@gmail.com>
2020-04-22 07:25:14 +0000

    PEP8 fixes

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -17,10 +17,12 @@ def hello_hbnb():
     """Displays a message when making a request to root."""
     return "Hello HBNB!"
 
+
 @app.route('/hbnb', strict_slashes=False)
 def simple_hbnb():
     """Display the string 'HBNB!'"""
     return "HBNB"
+
 
 if __name__ == "__main__":
     app.run()
