e891448f3f254cc1931edd011e1faa457db14a35
JamesPagani <jaimeagalvezv@gmail.com>
2020-04-22 06:01:42 +0000

    Making a request and passing a variable at the end of the URL path will display a string

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""C is fun!
+
+A bare bones Flask web application.
+Making a request to '/c/<text>' will print a string along with <text>.
+"""
+
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Display a message when making a request to root."""
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def simple_hbnb():
+    """Display the string 'HBNB!'"""
+    return "HBNB!"
+
+@app.route('/c/<text>', strict_slashes=False)
+def what_is_c(text):
+    """Display 'C is <text>', where text is your input text."""
+    return "C is {}".format(text)
+
+if __name__ == "__main__":
+    app.run()
