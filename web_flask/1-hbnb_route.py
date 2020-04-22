0ea7ad68969379674967ce4f5bcb86a5f9087d41
JamesPagani <jaimeagalvezv@gmail.com>
2020-04-22 05:52:53 +0000

    Making a request to another file displays another message

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""HBNB
+
+A bare bones Flask web application.
+Making a request to '/hbnb' displays a simple string.
+"""
+
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Displays a message when making a request to root."""
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def simple_hbnb():
+    """Display the string 'HBNB!'"""
+    return "HBNB!"
+
+if __name__ == "__main__":
+    app.run()
