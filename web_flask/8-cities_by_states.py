14edf0b186115a7fea09508f5308b0f01ddece12
= <=>
2020-04-25 06:45:44 +0000

    PEP8 fix: 2 blank lines expected between each routing

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -64,6 +64,7 @@ def states_list():
     states = storage.all(State).values()
     return render_template("7-states_list.html", states=states)
 
+
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
     states = storage.all(State).values()
